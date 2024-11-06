f2c0b9111bc6e6931fd384d3fa57751d7cc7e887
Nikita Lysenkov <138313293+Sograven@users.noreply.github.com>
2024-11-06 20:30:43 +0700

    Initialization

---
+++ Storge.Bot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Storge.Bot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
