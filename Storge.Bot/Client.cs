083a85bb382b90f06ad706c2707f411c0b04a819
Nikita Lysenkov <138313293+Sograven@users.noreply.github.com>
2024-11-10 21:03:53 +0700

    Basic functionality released

---
+++ Storge.Bot/Client.cs
@@ -0,0 +1,52 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Storge.Bot;
+
+public static class Client
+{
+    private static readonly CancellationTokenSource Cts;
+    private static readonly TelegramBotClient Bot;
+    
+    public static async Task Main()
+    {
+        await StartHandling();
+    }
+
+    private static async Task StartHandling()
+    {
+        var me = await Bot.GetMe();
+        Console.WriteLine(me.FirstName);
+        
+        Bot.OnUpdate += OnUpdate;
+        Bot.OnMessage += OnMessage;
+        Bot.OnError += OnError;
+
+        Console.ReadLine();
+        await Cts.CancelAsync();
+    }
+    
+    private static async Task OnUpdate(Update update)
+    {
+        
+    }
+    
+    private static async Task OnMessage(Message message, UpdateType updateType)
+    {
+        if (message.Text == "/start")
+            await Bot.SendMessage(message.Chat, $"Hi, {message.From!.FirstName}!");
+    }
+
+    private static async Task OnError(Exception exception, HandleErrorSource source)
+    {
+        Console.WriteLine(exception);
+    }
+
+    static Client()
+    {
+        Cts = new CancellationTokenSource();
+        Bot = new TelegramBotClient("<Token>", cancellationToken: Cts.Token);
+    }
+}
