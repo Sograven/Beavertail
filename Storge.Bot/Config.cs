7dfd591ebc3ea87bc6f9653f7ec9f4c788d30210
Nikita Lysenkov <138313293+Sograven@users.noreply.github.com>
2024-11-13 21:10:12 +0700

    Environment variables fix

--- Storge.Bot/Config.cs
+++ Storge.Bot/Config.cs
@@ -6,16 +6,14 @@ public static class Config
     {
         get
         {
-            // ReSharper disable once InvertIf
-            if (Environment.GetEnvironmentVariable("STORGEBOT_TOKEN") is null)
-            {
-                Console.Write("System variable STORGEBOT_TOKEN doesn't set.\n" +
-                              "Please, enter your 'Telegram Bot API' token: ");
-                
-                var token = Console.ReadLine();
-                Environment.SetEnvironmentVariable("STORGEBOT_TOKEN", token, EnvironmentVariableTarget.User);
-            }
+            if (Environment.GetEnvironmentVariable("STORGEBOT_TOKEN") is not null)
+                return Environment.GetEnvironmentVariable("STORGEBOT_TOKEN")!;
             
+            Console.Write("System variable STORGEBOT_TOKEN doesn't set.\n" +
+                          "Please, enter your 'Telegram Bot API' token: ");
+            var token = Console.ReadLine();
+            Environment.SetEnvironmentVariable("STORGEBOT_TOKEN", token, EnvironmentVariableTarget.User);
+
             return Environment.GetEnvironmentVariable("STORGEBOT_TOKEN")!;
         }
     }
