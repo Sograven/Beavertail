d2e6876082ac2e192447e2dbd1fcd4950e45aab1
Nikita Lysenkov <138313293+Sograven@users.noreply.github.com>
2024-11-29 04:21:42 +0700

    #10 refactoring; issue is closed

--- Storge.Bot/Config.cs
+++ Storge.Bot/Config.cs
@@ -2,8 +2,14 @@ namespace Storge.Bot;
 
 public static class Config
 {
+    /// <summary>
+    /// Path to bot's configuration file.
+    /// </summary>
     private static readonly string _path;
 
+    /// <summary>
+    /// Returns bot's token from configuration file.
+    /// </summary>
     public static string Token
     {
         get
@@ -11,16 +17,12 @@ public static class Config
             if (!Path.Exists(_path))
             {
                 Console.Write("Config file doesn't exist. Initialization...\n" +
-                                  "Please, enter bot token: ");
+                              "Please, enter bot token: ");
 
                 var token = Console.ReadLine();
                 File.WriteAllText(_path, token);
-            }
-            else if (File.ReadAllText(_path) is "")
-            {
-                Console.Write("Please, enter bot token: ");
-                var token = Console.ReadLine();
-                File.WriteAllText(_path, token);
+
+                Console.WriteLine("Your configuration file is located at:\n" + _path);
             }
 
             return File.ReadAllText(_path);
