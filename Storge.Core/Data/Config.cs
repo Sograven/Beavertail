ea5d83c8797786e4f1c3a6a0578a0e190df6f501
Nikita Lysenkov <138313293+Sograven@users.noreply.github.com>
2024-12-19 17:26:44 +0700

    #14; Create configuration file for db

---
+++ Storge.Core/Data/Config.cs
@@ -0,0 +1,40 @@
+﻿namespace Storge.Core.Data;
+
+/// <summary>
+/// Contains database files paths and parameters.
+/// </summary>
+internal static class Config
+{
+    /// <summary>
+    /// Gets the path to file with users data.
+    /// </summary>
+    internal static string UsersFilePath { get; } = GetFilePath("SCD_USERS", "users.db");
+
+    /// <summary>
+    /// Retrieves a path to data file from environment variable.
+    /// </summary>
+    /// <param name="variableName">Environment variable name.</param>
+    /// <param name="fileName">Name of the file.</param>
+    /// <returns>Path to data file.</returns>
+    private static string GetFilePath(string variableName, string fileName)
+    {
+        if (Environment.GetEnvironmentVariable(variableName) is null)
+            SetFilePath(variableName, fileName);
+
+        return Environment.GetEnvironmentVariable(variableName)!;
+    }
+
+    /// <summary>
+    /// Creates an environment variable storing path to data file.
+    /// </summary>
+    /// <param name="variableName">Environment variable name.</param>
+    /// <param name="fileName">Name of the file.</param>
+    private static void SetFilePath(string variableName, string fileName)
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var folderPath = Environment.GetFolderPath(folder);
+        
+        var path = Path.Combine(folderPath, fileName);
+        Environment.SetEnvironmentVariable(variableName, path);
+    }
+}
